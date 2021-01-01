14caac880d5be91947b9fbe1e8567902ee624321
Chefcury1 <exploitload@gmail.com>
2021-01-01 17:57:15 +0000

    Integrated multiprocessing module for multiple folder monitoring

--- src/main.py
+++ src/main.py
@@ -2,43 +2,21 @@
 
 import os, sys
 import time
+import multiprocessing
 # Use pip3 install watchdog to install watchdog 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-# directories_to_watch = ["C:\\Users\\explo\\Documents", "C:\\xampp\\htdocs", "F:\\laravel-apps", "F:\\All Courses Mike"]
-directories_to_watch = "C:\\Users\\explo\\Documents"
+directories_to_watch = ["C:\\Users\\explo\\Documents", "C:\\xampp\\htdocs", "F:\\laravel-apps", "F:\\All Courses Mike"]
 path_to_write = "C:\\Users\\explo\\Desktop"
 file_name = "filesystem_monitoring.txt"
 storage_path = path_to_write + '\\' + file_name
-
-if not os.path.exists(directories_to_watch):
-    print("The Directory/Path does not exist")
-    sys.exit(1)
 
 if not os.path.exists(path_to_write):
     print("The Path does not exist")
     sys.exit(1)
 
-
-class Monitor:
-    def __init__(self, watch_directory):
-        self.observer = Observer()
-        self.watch_directory = watch_directory
-
-    def run(self):
-        event_handler = Handler()
-        self.observer.schedule(event_handler, self.watch_directory, recursive=True)
-        self.observer.start()
-        try:
-            while True:
-                time.sleep(10)
-        except:
-            self.observer.stop()
-            print(f"[+] Saved output to {path_to_write}\{file_name}")
-            print("[-] Stopped Observer and Caught Exception.")
-        self.observer.join()
 
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
@@ -51,19 +29,37 @@ class Handler(FileSystemEventHandler):
         with open(storage_path, 'a') as file:
             file.write(f"[!!] Deleted {event.src_path}")
             file.write("\n")
-    # Excluded modified function cause of inconsistency, e.g
-    # For every creation and deletion there is a modification,
-    # and for every real modification there is a modification,
-    # thus inconsistency in determining the real modification.
-    # def on_modified(self, event):
-    #     # Take any action here when a file is first modified.
-    #     print(f"on_modified {event.src_path}")
     def on_moved(self, event):
         # Take any action here when a file is first moved (Renamed).
         with open(storage_path, 'a') as file:
             file.write(f"[>>] Renamed {event.src_path} to {event.dest_path}")
             file.write("\n")
 
+event_handler = Handler()
+observer = Observer()
+
+def dir_watcher(watch_directory):
+    observer.schedule(event_handler, watch_directory, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+        print(f"[+] Saved output to {path_to_write}\{file_name}")
+        print("[-] Stopped Observer and Caught Exception.")
+    observer.join()
+
+def stop_observer():
+    observer.stop()
+    observer.join()
+    sys.exit()
+
+
 if __name__ == '__main__':
-    m = Monitor(directories_to_watch)
-    m.run()
+    for watch_directory in directories_to_watch:
+        if not os.path.exists(watch_directory.strip(' ')):
+            print(f"The Directory/Path {watch_directory} does not exist")
+            sys.exit(1)
+        mp = multiprocessing.Process(target=dir_watcher, args=(watch_directory.strip(' '),))
+        mp.start()
