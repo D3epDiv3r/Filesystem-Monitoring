5d542cdb7f4e3991345b7f4acebf23e44380f753
Chefcury1 <exploitload@gmail.com>
2021-01-02 15:29:25 +0000

    Refactored code to include TKinter window as Control center

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import tkinter
+from tkinter import messagebox as msg
 import os, sys
+import signal
 import time
 import multiprocessing
 # Use pip3 install watchdog to install watchdog 
@@ -16,7 +19,6 @@ storage_path = path_to_write + '\\' + file_name
 if not os.path.exists(path_to_write):
     print("The Path does not exist")
     sys.exit(1)
-
 
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
@@ -37,6 +39,7 @@ class Handler(FileSystemEventHandler):
 
 event_handler = Handler()
 observer = Observer()
+window = tkinter.Tk()
 
 def dir_watcher(watch_directory):
     observer.schedule(event_handler, watch_directory, recursive=True)
@@ -47,19 +50,42 @@ def dir_watcher(watch_directory):
     except KeyboardInterrupt:
         observer.stop()
         print(f"[+] Saved output to {path_to_write}\{file_name}")
-        print("[-] Stopped Observer and Caught Exception.")
+        print("[-] Stopped Observer and Caught Exception (KeyboardInterrupt).")
     observer.join()
 
-def stop_observer():
-    observer.stop()
-    observer.join()
-    sys.exit()
-
-
-if __name__ == '__main__':
+process_ids = []
+def start_observer():
+    global process_ids
+    msg.showinfo('Starting Observer', 'Monitoring will begin in the background if path(s) exist')
     for watch_directory in directories_to_watch:
         if not os.path.exists(watch_directory.strip(' ')):
-            print(f"The Directory/Path {watch_directory} does not exist")
-            sys.exit(1)
+            msg.showerror('Path Error!', f"The Directory or Path {watch_directory} does not exist. Restart program after fixing path.")
+            break
         mp = multiprocessing.Process(target=dir_watcher, args=(watch_directory.strip(' '),))
         mp.start()
+        process_ids.append(mp.pid)
+
+def stop_observer():
+    global process_ids
+    for kill_id in process_ids:
+        os.kill(kill_id, signal.SIGINT)
+    process_ids = []
+    msg.showinfo('Stopping Observer', f"[+] All Observers have been stopped. Saved output to {path_to_write}\{file_name}")
+
+def open_window():
+    window.title("Filesystem Monitoring")
+    app_width = 400
+    app_height = 300
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width / 2) - (app_width / 2)
+    y = (screen_height / 2) - (app_height / 2)
+    window.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
+    b1 = tkinter.Button(window, text="Start monitoring", command=start_observer, fg="green")
+    b2 = tkinter.Button(window, text="Stop monitoring", command=stop_observer, fg="red")
+    b1.place(relx = 0.5, rely = 0.4, anchor = "center")
+    b2.place(relx = 0.5, rely = 0.6, anchor = "center")
+    window.mainloop()
+
+if __name__ == '__main__':
+    open_window()
