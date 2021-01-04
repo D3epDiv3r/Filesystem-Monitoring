765ff88f5ecff9da6beca909178b23ab47a56bed
Chefcury1 <exploitload@gmail.com>
2021-01-04 03:00:46 +0000

    Added functionality to launch program on Start Up

--- src/main.py
+++ src/main.py
@@ -10,33 +10,47 @@ import os, sys
 import signal
 import time
 import multiprocessing
+from pathlib import Path
+from PIL import Image, ImageTk
 # Use pip3 install watchdog to install watchdog 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
 directories_to_watch = ["C:\\Users\\explo\\Documents", "C:\\xampp\\htdocs", "F:\\laravel-apps", "F:\\All Courses Mike"]
-path_to_write = "C:\\Users\\explo\\Desktop"
+path_to_write = "C:\\Users\\explo\\Desktop" # Ensure path is not included in watched directory above
 file_name = "filesystem_monitoring.txt"
 storage_path = path_to_write + '\\' + file_name
+# Replace path to "main.py" with your systems path to "main.py" to start program when you Login
+start_file_on_login_path = "C:\\Users\\explo\\Documents\\Python_Projects\\filesystem_monitoring\\src\\main.py"
+# Replace with your startup folder path -> press WinKey + R, then type shell:startup and hit enter to get path.
+startup_path = 'C:\\Users\\explo\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
 
-if not os.path.exists(path_to_write):
-    msg.showerror('Path Error!', f"[ !! ]The Path to write directory {path_to_write} does not exist.")
-    sys.exit(1)
+def check_path_exists(path):
+    if not os.path.exists(path):
+        msg.showerror('Path Error!', f"[ !! ]The Path to write directory {path} does not exist.")
+        sys.exit(1)
+    else:
+        pass
+
+# Check that paths exists
+check_path_exists(path_to_write)
+check_path_exists(start_file_on_login_path)
+check_path_exists(startup_path)
 
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
-        # Take any action here when a file is first created.
+        # Take any action here when a file is created.
         with open(storage_path, 'a') as file:
             file.write(f"[++] Created {event.src_path}")
             file.write("\n")
     def on_deleted(self, event):
-        # Take any action here when a file is first deleted.
+        # Take any action here when a file is deleted.
         with open(storage_path, 'a') as file:
             file.write(f"[!!] Deleted {event.src_path}")
             file.write("\n")
     def on_moved(self, event):
-        # Take any action here when a file is first moved (Renamed).
+        # Take any action here when a file is moved (Renamed).
         with open(storage_path, 'a') as file:
             file.write(f"[>>] Renamed {event.src_path} to {event.dest_path}")
             file.write("\n")
@@ -45,6 +59,10 @@ b1 = ''
 event_handler = Handler()
 observer = Observer()
 window = tk.Tk()
+
+def add_to_startup(startup_path, file_path):
+    with open(startup_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(f'python {file_path}')
 
 def dir_watcher(watch_directory):
     observer.schedule(event_handler, watch_directory, recursive=True)
@@ -113,4 +131,5 @@ def open_window():
     window.mainloop()
 
 if __name__ == '__main__':
+    add_to_startup(startup_path, start_file_on_login_path)
     open_window()
