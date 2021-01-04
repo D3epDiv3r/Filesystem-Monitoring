4d5c94919486e008439cca3c52417150d5f474f4
Chefcury1 <exploitload@gmail.com>
2021-01-04 16:22:51 +0000

    Added Docstrings for functions and included directional comments

--- src/main.py
+++ src/main.py
@@ -10,21 +10,29 @@ import os, sys
 import signal
 import time
 import multiprocessing
-# Use pip3 install watchdog to install watchdog 
+# Use "pip3 install watchdog" to install the watchdog library 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
+# NOTE: GENERAL FOLDER NAMING CONVENTION:
+### NOTE -- Ensure to include "2 (TWO)" backslash in ALL PATHS BELOW 
+### NOTE -- e.g "C:\Users\explo\Desktop" should be "C:\\Users\\explo\\Desktop"
+
+# Include directory or directories you want to moitor within double quotes and square brackets: directories_to_watch = ["your_dir"]
+# Seperate Multiple directories with comma ',' e.g directories_to_watch = ["C:\\your_dir_1", "F:\\your_dir_2"]
 directories_to_watch = ["C:\\Users\\explo\\Documents", "C:\\xampp\\htdocs", "F:\\laravel-apps", "F:\\All Courses Mike"]
-path_to_write = "C:\\Users\\explo\\Desktop" # Ensure path is not included in watched directory above
-file_name = "filesystem_monitoring.txt"
+path_to_write = "C:\\Users\\explo\\Desktop" # Ensure this path is not included in watched directory above
+file_name = "filesystem_monitoring.txt" # The filename you want to use to store the event log
 storage_path = path_to_write + '\\' + file_name
-# Replace path to "main.py" with your systems path to "main.py" to start program when you Login
+# Replace path to this "main.py" file with your systems path to "main.py" to start program when you Login
 start_file_on_login_path = "C:\\Users\\explo\\Documents\\Python_Projects\\filesystem_monitoring\\src\\main.py"
 # Replace with your startup folder path -> press WinKey + R, then type shell:startup and hit enter to get path.
 startup_path = 'C:\\Users\\explo\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
 
 def check_path_exists(path):
+    """ This function simply checks that the paths provided exists. 
+    If path exists it simply passes else it exits with exit status 1 """
     if not os.path.exists(path):
         msg.showerror('Path Error!', f"[ !! ]The Path to write directory {path} does not exist.")
         sys.exit(1)
@@ -37,6 +45,9 @@ check_path_exists(start_file_on_login_path)
 check_path_exists(startup_path)
 
 class Handler(FileSystemEventHandler):
+    """ A subclass of Watchdog's FileSystemEventHandler class. 
+    It listens for the events below and carries out an action
+    based on the event that was fired. """
     def on_created(self, event):
         # Take any action here when a file is created.
         with open(storage_path, 'a') as file:
@@ -53,16 +64,23 @@ class Handler(FileSystemEventHandler):
             file.write(f"[>>] Renamed {event.src_path} to {event.dest_path}")
             file.write("\n")
 
+# Global button for handling state
 b1 = ''
+# Initialize event handler
 event_handler = Handler()
+# Initialize Watchdog's Observer
 observer = Observer()
+# Initialize Tkinter window
 window = tk.Tk()
 
 def add_to_startup(startup_path, file_path):
+    """ This function makes program Startup on Login"""
     with open(startup_path + '\\' + "open.bat", "w+") as bat_file:
         bat_file.write(f'python {file_path}')
 
 def dir_watcher(watch_directory):
+    """ Schedules an Observer on directories specified in 
+    global directories_to_watch list. """
     observer.schedule(event_handler, watch_directory, recursive=True)
     observer.start()
     try:
@@ -76,6 +94,8 @@ def dir_watcher(watch_directory):
 
 process_ids = []
 def start_observer():
+    """ Starts an Observer on each directory specified in its own process
+    using the multiprocess module and returns process ids for further handling. """
     global b1
     global process_ids
     msg.showinfo('Starting Observer', 'Monitoring will begin in the background if path(s) exist')
@@ -92,6 +112,8 @@ def start_observer():
         process_ids.append(mp.pid)
 
 def stop_observer():
+    """ Kills all the running processes associated with Observers
+    thus, stops all monitoring. """
     global b1
     global process_ids
     for kill_id in process_ids:
@@ -103,6 +125,8 @@ def stop_observer():
     msg.showinfo('Stopping Observer', f"[ - ] All Observers have been stopped.")
 
 def exit_application():
+    """ Handles Tkinter default close (X) and Alt + F4 response 
+    and returns customized response. """
     response = msg.askyesno('Close Application', f'Are you sure you want to exit the application? \n\n "YES" - by clicking "Yes", the app will close and all observers will be stopped. \n\n "NO" - by clicking "No", the app will be minimized to the taskbar and continue running in the background', icon='warning')
     if response == True:
         stop_observer()
@@ -112,6 +136,8 @@ def exit_application():
         window.iconify()
 
 def open_window():
+    """ Creates Tkinter window, start and stop buttons, and 
+    specifies new protocol for Close (X) window event. """
     global b1
     window.title("Filesystem Monitoring")
     app_width = 400
@@ -129,5 +155,7 @@ def open_window():
     window.mainloop()
 
 if __name__ == '__main__':
+    # Add program to start up
     add_to_startup(startup_path, start_file_on_login_path)
+    # Open Tkinter window / Run program
     open_window()
